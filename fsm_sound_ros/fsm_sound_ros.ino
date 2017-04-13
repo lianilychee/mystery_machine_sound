703c091ce301c781e4f7d7b2fb8eff57508e2078
Liani Lye <llye@ivani.com>
2017-04-13 14:47:22 -0400

    ADD: decision making around choosing between elevator music soundtracks

--- fsm_sound_ros/fsm_sound_ros.ino
+++ fsm_sound_ros/fsm_sound_ros.ino
@@ -30,21 +30,28 @@ ros::Subscriber<std_msgs::Int16> bot_state_sub("bot_state", &audioCb);
  * 
  * Index    Filename        State(str)     Action                   Audio Clip
  * -----    --------        ---------      ---------                ----------
- * 00001    TRACK004.mp3    0: within      playing elev music       *elevator music 1*
+ * 00001    TRACK004.mp3    0: within      playing elev music       Never Gonna Give You Up
  * 00002    TRACK001.mp3    1: asking      requesting aid @ elev    "Hello. Can you pls call elev for me?"
  * 00003    TRACK002.mp3    2: entering    entering elev            "Entering elevator; pls stand clear."
- * 00004    TRACK003.mp3    3: exiting"    exiting elev             "Exiting elevator; pls stand clear."
- * 
+ * 00004    TRACK003.mp3    3: exiting     exiting elev             "Exiting elevator; pls stand clear."
+ * 00005    TRACK005.mp3    0: within      playing elev music       generic elevator music
+ * 00006    TRACK006.mp3    0: within      playing elev music       Scooby Doo theme song
  */
 void audioCb( int std_msgs::Int16& state ) {
 
   char audio_index;
+  int track_val;
 
   // Convert state to appropriate audio index.
   if (state == 1) audio_index = '00002';
   else if (state == 2) audio_index = '00003';
   else if (state == 3) audio_index = '00004';
-  else if (state == 0) audio_index = '00001';
+  else if (state == 0) {
+    track_val random(0,2);
+    if (track_val == 0) audio_index = '00001';
+    else if (track_val == 1) audio_index = '00005';
+    else if (track_val == 2) audio_index = '00006';
+  }
 
   parse_menu(audio_index);    // Send audio_index to parse_menu() to play appropriate track.
 
